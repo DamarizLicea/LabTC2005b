64380d1e22037a636c157f4aeace589cedee1822
Damariz Licea <A01369045@itesm.mx>
2023-03-05 21:02:32 -0600

    Laboratorio terminado

--- L12/routes/keikiz.routes.js
+++ L12/routes/keikiz.routes.js
@@ -8,7 +8,8 @@ const keikiz = [
   {
     nombre: "Chocolate Húmedo",
     //usar imagen estatica de la carpeta public llamada humedo.jpg
-    imagen: "/humedo.jpg",
+    imagen:
+      "https://ohsweetbasil.com/wp-content/uploads/chocolate-cake-with-chocolate-frosting-recipe-13-1024x1536.jpg",
     descripcion: "Receta muy rendidora tipo pinguinos",
     handle: "@humedo",
     ingredientes: "harina, huevos, mayonesa, cocoa, bicarbonato, azúcar",
@@ -16,7 +17,8 @@ const keikiz = [
   },
   {
     nombre: "Gansito",
-    imagen: "/gansito.jpg",
+    imagen:
+      "https://cdn.shopify.com/s/files/1/0567/7849/0010/products/WhatsAppImage2022-03-22at10.11.42AM_720x.jpg?v=1647966335",
     descripcion: "Añadir mermelada de fresa y crema chantilly",
     handle: "@gansito",
     ingredientes:
@@ -25,7 +27,8 @@ const keikiz = [
   },
   {
     nombre: "Vainilla",
-    imagen: "/vainilla.jpg",
+    imagen:
+      "https://www.allrecipes.com/thmb/B4ncMNu-G9XgfqtyIKYiT6TcWd0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/277000-easy-vanilla-cake-ddmfs-3X4-0103-09ae059661e5407599625222c5ac7d3b.jpg",
     descripcion: "Bizcocho tipo esponja muy versátil",
     handle: "@vainilla",
     ingredientes:
@@ -34,7 +37,8 @@ const keikiz = [
   },
   {
     nombre: "Limón",
-    imagen: "/limon.jpg",
+    imagen:
+      "https://livforcake.com/wp-content/uploads/2021/06/lemon-cake-thumb-2021-1.jpg",
     descripcion: "Añadir lemon curd y merengue",
     handle: "@limon",
     ingredientes:
@@ -43,7 +47,8 @@ const keikiz = [
   },
   {
     nombre: "Red Velvet",
-    imagen: "galery/redvelvet.jpg",
+    imagen:
+      "https://thescranline.com/wp-content/uploads/2021/03/Red-Velvet-Cake.jpg",
     descripcion: "Usar buttercream de queso crema",
     handle: "@red_velvet",
     ingredientes:
@@ -52,7 +57,8 @@ const keikiz = [
   },
   {
     nombre: "Zanahoria",
-    imagen: "galery/zanahoria.jpg",
+    imagen:
+      "https://www.janespatisserie.com/wp-content/uploads/2015/08/CarrotCake1-768x1152.jpg",
     descripcion:
       "Usar buttercream de queso crema y decorar con formas de zanahoria",
     handle: "@zanahoria",
@@ -62,7 +68,8 @@ const keikiz = [
   },
   {
     nombre: "Chocomenta",
-    imagen: "galery/chocomenta.jpg",
+    imagen:
+      "https://livforcake.com/wp-content/uploads/2017/05/mint-chocolate-chip-cake-2.jpg",
     descripcion: "Usar buttercream con menta y uno de chocolate",
     handle: "@chocomenta",
     ingredientes:
@@ -71,7 +78,8 @@ const keikiz = [
   },
   {
     nombre: "Chai",
-    imagen: "galery/random.jpg",
+    imagen:
+      "https://www.thelittleepicurean.com/wp-content/uploads/2019/03/chai-layer-cake-1-1.jpg",
     descripcion: "Usar buttercream de queso crema",
     handle: "@chai",
     ingredientes:
